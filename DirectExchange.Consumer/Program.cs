d668af776f6deb5e40ec7512381aa98ebc626a25
mohammedelmoghazi <mohammed.elmoghazi@alareebict.com>
2024-10-29 22:27:52 +0300

    Add project files.

---
+++ DirectExchange.Consumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace DirectExchange.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            Console.WriteLine("Waiting for messages");
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                //here to do some process
+
+                Console.WriteLine($"recieved message {message}");
+            };
+
+
+            channel.BasicConsume(queue: "q-new-classes", autoAck: true, consumer: consumer);
+
+            Console.WriteLine("Prss Enter key to close");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DirectExchange.Producer/Program.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace DirectExchange.Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            const string message = "Class-B";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            channel.BasicPublish(exchange: "amq.direct",
+                routingKey: "rk-new-classes",
+                basicProperties: null,
+                body: body);
+ //           channel.Close();
+            Console.WriteLine($"sent {message}");
+            Console.WriteLine("Prss Enter key to close");
+            Console.ReadKey();
+        }
+    }
+}
